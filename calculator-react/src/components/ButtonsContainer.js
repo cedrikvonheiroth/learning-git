edd76e4ec9ac3a6e3ba0cc19bfb5c0ffefe05273
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-02-16 14:41:04 +0100

    Made some looping in ButtonsContainer

--- calculator-react/src/components/ButtonsContainer.js
+++ calculator-react/src/components/ButtonsContainer.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useState } from 'react';
 import { buttons } from "../css/ButtonsContainer.module.css"
 
 function ButtonsContainer() {
+    const [buttonValues, setButtonValues] = useState([
+        7, 8, 9, "/", 4, 5, 6, "x", 1, 2, 3, "-", "clear", 0, "+", "="
+    ]);
+
     return (
         <div className={buttons}>
-            <p>we will map buttons later</p>
+            {buttonValues.map((value) => (
+                <p key={value}>{value}</p>
+            ))}
         </div>
-    )
+    );
 }
 
 export default ButtonsContainer
