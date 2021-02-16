1883ef3dff7f852a173e3862063e2c7dae5731b2
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-02-16 13:41:17 +0100

    Created Calculator component

---
+++ calculator-react/src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { calculator } from "..css/Calculator.module.css"
+
+function Calculator() {
+    return (
+        <div className={calculator}>
+            
+        </div>
+    )
+}
+
+export default Calculator
